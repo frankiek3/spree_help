333a57f16f60e45cd28a8c1b7e7202477e3d9881
Frank <frankiek3@gmail.com>
2015-05-05 20:00:58 -0400

    Update

--- lib/spree_help/engine.rb
+++ lib/spree_help/engine.rb
@@ -4,18 +4,13 @@ module SpreeHelp
     isolate_namespace Spree
     engine_name 'spree_help'
 
-    config.autoload_paths += %W(#{config.root}/lib)
-
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/overrides/*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
     end
 
+    config.autoload_paths += %W(#{config.root}/lib)
     config.to_prepare &method(:activate).to_proc
   end
 end
